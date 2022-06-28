8d3cf3ab10a209a8123e3db26eb99ebe1994b0e6
Tabot Charles Bessong <ebezebeatrice@gmail.com>
2022-06-28 12:27:21 +0100

    created the mock data

---
+++ frontend/src/constant/data.js
@@ -0,0 +1,285 @@
+
+import images from "./images";
+
+
+const bus = [
+  {
+    id:1,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:'musango',
+    img:images.bus201
+  },
+
+  {
+    id:2,
+    origin:'Buea',
+    destination:'Yaounde',
+    price:'5000',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus701
+  },
+
+  {
+    id:3,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1000',
+    capacity:30,
+    agency:'mondial',
+    img:images.bus301
+  },
+
+  {
+    id:4,
+    origin:'Douala',
+    destination:'Yaounde',
+    price:'3500',
+    capacity:40,
+    agency:'express',
+    img:images.bus401
+  },
+
+  {
+    id:5,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:null,
+    img:images.bus1
+  },
+
+  {
+    id:6,
+    origin:'Douala',
+    destination:'Ngaoundere',
+    price:'6000',
+    capacity:100,
+    agency:'mondial',
+    img:images.bus101
+  },
+
+  {
+    id:7,
+    origin:'Buea',
+    destination:'Limbe',
+    price:'600',
+    capacity:70,
+    agency:'musango',
+    img:images.bus2
+  },
+
+  {
+    id:8,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:'express',
+    img:images.bus202
+  },
+
+  {
+    id:9,
+    origin:'Buea',
+    destination:'Yaounde',
+    price:'5000',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus701
+  },
+
+  {
+    id:10,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1000',
+    capacity:30,
+    agency:'mondial',
+    img:images.bus301
+  },
+
+  {
+    id:11,
+    origin:'Douala',
+    destination:'Yaounde',
+    price:'3500',
+    capacity:40,
+    agency:'express',
+    img:images.bus401
+  },
+
+  {
+    id:12,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1300',
+    capacity:30,
+    agency:null,
+    img:images.bus303
+  },
+
+  {
+    id:13,
+    origin:'Douala',
+    destination:'Ngaoundere',
+    price:'6000',
+    capacity:100,
+    agency:'mondial',
+    img:images.bus101
+  },
+
+  {
+    id:14,
+    origin:'Buea',
+    destination:'Limbe',
+    price:'600',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus704
+  },
+
+  {
+    id:15,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:'musango',
+    img:images.bus203
+  },
+
+  {
+    id:16,
+    origin:'Buea',
+    destination:'Yaounde',
+    price:'5000',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus704
+  },
+
+  {
+    id:17,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1000',
+    capacity:30,
+    agency:'mondial',
+    img:images.bus304
+  },
+
+  {
+    id:18,
+    origin:'Douala',
+    destination:'Yaounde',
+    price:'3500',
+    capacity:40,
+    agency:'express',
+    img:images.bus404
+  },
+
+  {
+    id:19,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:null,
+    img:images.bus204
+  },
+
+  {
+    id:20,
+    origin:'Douala',
+    destination:'Ngaoundere',
+    price:'6000',
+    capacity:100,
+    agency:'mondial',
+    img:images.bus103
+  },
+
+  {
+    id:21,
+    origin:'Buea',
+    destination:'Limbe',
+    price:'600',
+    capacity:70,
+    agency:'musango',
+    img:images.bus704
+  },
+
+  {
+    id:22,
+    origin:'Buea',
+    destination:'Douala',
+    price:'1500',
+    capacity:20,
+    agency:'express',
+    img:images.bus206
+  },
+
+  {
+    id:23,
+    origin:'Buea',
+    destination:'Yaounde',
+    price:'5000',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus706
+  },
+
+  {
+    id:24,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1000',
+    capacity:30,
+    agency:'mondial',
+    img:images.bus306
+  },
+
+  {
+    id:25,
+    origin:'Douala',
+    destination:'Yaounde',
+    price:'3500',
+    capacity:40,
+    agency:'express',
+    img:images.bus406
+  },
+
+  {
+    id:26,
+    origin:'Douala',
+    destination:'Buea',
+    price:'1300',
+    capacity:30,
+    agency:null,
+    img:images.bus306
+  },
+
+  {
+    id:27,
+    origin:'Douala',
+    destination:'Ngaoundere',
+    price:'6000',
+    capacity:100,
+    agency:'mondial',
+    img:images.bus104
+  },
+
+  {
+    id:28,
+    origin:'Buea',
+    destination:'Limbe',
+    price:'600',
+    capacity:70,
+    agency:'mondial',
+    img:images.bus703
+  },
+]
---
+++ frontend/src/constant/images.js
@@ -0,0 +1,80 @@
+
+import bus201 from '../images/20-1.jfif'
+import bus202 from '../images/20-2.jfif'
+import bus203 from '../images/20-3.jfif'
+import bus204 from '../images/20-4.jfif'
+import bus205 from '../images/20-5.jfif'
+import bus206 from '../images/20-6.jpeg'
+import bus301 from '../images/30-1.jfif'
+import bus302 from '../images/30-2.jfif'
+import bus303 from '../images/30-3.jfif'
+import bus304 from '../images/30-4.jfif'
+import bus305 from '../images/30-5.jfif'
+import bus306 from '../images/30-6.jfif'
+import bus401 from '../images/40-1.jfif'
+import bus402 from '../images/40-2.jfif'
+import bus403 from '../images/40-3.jfif'
+import bus404 from '../images/40-4.jfif'
+import bus405 from '../images/40-5.jfif'
+import bus406 from '../images/40-6.jfif'
+import bus601 from '../images/60-1.jpg'
+import bus602 from '../images/60-2.jpeg'
+import bus603 from '../images/60-3.jfif'
+import bus604 from '../images/60-4.jpg'
+import bus605 from '../images/60-5.jpeg'
+import bus606 from '../images/60-6.jfif'
+import bus701 from '../images/70-1.jpg'
+import bus702 from '../images/70-2.jfif'
+import bus703 from '../images/70-3.jfif'
+import bus704 from '../images/70-4.jfif'
+import bus705 from '../images/70-5.jfif'
+import bus706 from '../images/70-6.jfif'
+import bus101 from '../images/100-1.jpg'
+import bus102 from '../images/100-2.jpg'
+import bus103 from '../images/100-3.jfif'
+import bus104 from '../images/100-4.jpg'
+import bus1 from '../images/bus1.jfif'
+import bus2 from '../images/bus3.jfif'
+import bus3 from '../images/bus3.jpg'
+
+
+export default {
+  bus201,
+  bus202,
+  bus203,
+  bus204,
+  bus205,
+  bus206,
+  bus301,
+  bus302,
+  bus303,
+  bus304,
+  bus305,
+  bus306,
+  bus401,
+  bus402,
+  bus403,
+  bus404,
+  bus405,
+  bus406,
+  bus601,
+  bus602,
+  bus603,
+  bus604,
+  bus605,
+  bus606,
+  bus701,
+  bus702,
+  bus703,
+  bus704,
+  bus705,
+  bus706,
+  bus101,
+  bus102,
+  bus103,
+  bus104,
+  bus1,
+  bus2,
+  bus3
+
+}
